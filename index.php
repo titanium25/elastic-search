1ca093eb9eb8e6747fbb359cb960129688ca6148
alexTriolla <alex@triolla.io>
2024-09-26 20:45:02 +0300

    Refactor index.php to remove unnecessary code and improve readability

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Check if the GET request contains a 'name' parameter
-if (isset($_GET['name'])) {
-    $name = htmlspecialchars($_GET['name']);
-    echo "Hello, " . $name . "!";
-} else {
-    echo "Hello, World!";
-}
-?>
